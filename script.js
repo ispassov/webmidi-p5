d92e0ef85059554c274a3010783a9f52c1ae4fb8
ispassov <61962940+ispassov@users.noreply.github.com>
2023-06-27 21:27:16 +0300

    1 file modified

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
-WebMidi.enable()
-  .then(onEnabled)
-  .catch(err => alert(err))
+WebMidi.enable().then(() => {
 
-function onEnabled() {
-  if (WebMidi.inputs.length < 1) {
-    console.log('No device detected.')
-  } else {
-    console.log(WebMidi.inputs[0])
+  const activeNotes = new Set()
+
+  WebMidi.inputs.forEach((input) => {
+    input.addListener('noteon', 'all', (event) => {
+      activeNotes.add(event.note.name)
+      detectChord()
+      console.log(activeNotes)
+    })
+  })
+
+
+  WebMidi.inputs.forEach((input) => {
+    input.addListener('noteoff', 'all', (event) => {
+      activeNotes.delete(event.note.name)
+      detectChord()
+    })
+  })
+
+  function detectChord() {
+    const activeNotesArray = Array.from(activeNotes)
+    if (activeNotesArray.length >= 3) {
+      const chordName = getChordName(activeNotesArray)
+      console.log('Detected Chord:', chordName)
+    }
   }
 
-  WebMidi.inputs[0].channels[1].addListener('noteon', e => {
-    console.log(e.velocity)
-    console.log(e.note.identifier)
-  })
-}
+  function getChordName(notes) {
+    if (
+      notes.includes('C') /* C */ &&
+      notes.includes('E') /* E */ &&
+      notes.includes('G') /* G */
+    ) {
+      return 'C Major'
+    }
+
+    if (
+      notes.includes(60) /* C */ &&
+      notes.includes(63) /* E */ &&
+      notes.includes(67) /* G */
+    ) {
+      return 'C Minor'
+    }
+
+    return 'Unknown'
+  }
+}).catch((error) => {
+  console.error('Web MIDI could not be enabled:', error)
+})
